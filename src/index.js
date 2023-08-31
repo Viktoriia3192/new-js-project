576caaa1931209765ea73265ab327249df2cd081
Viktoriia3192 <vikysja9231@gmail.com>
2023-08-31 12:20:22 +0100

    7com

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import './js/header_hrebinchuk';
 import './js/dark_theme_max';
 import './js/autorization_julia';
@@ -7,4 +9,4 @@ import './js/support_daryna';
 import './js/best_sellers_lipskii';
 import './js/modal_andrii';
 import './js/pagination_mariia';
-import './js/shopping_list_olexandr';
+import './shopping_list_olexandr';
