fe36a2e63ff983f3098c3fae8f50b41cb5520a51
Viktoriia3192 <vikysja9231@gmail.com>
2023-09-01 10:38:43 +0100

    18com

---
+++ src/shopping_list_olexandr.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+import './js/header_hrebinchuk';
+import './js/support_daryna';
+import './js/modal_andrii';
+import './js/dark_theme_max';
+import './js/pagination_mariia';
