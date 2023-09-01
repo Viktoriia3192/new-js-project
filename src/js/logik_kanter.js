5ed5330ba2ff546dfd2e5fd98e462c9a81ebf0b5
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-09-01 16:05:09 +0200

    Added error handling
    Added error handling during a request to the server. If the request was unsuccessful - we output a corresponding message to the console for now. So if the length of the book array is 0 - output a message to the console.

--- src/js/logik_kanter.js
+++ src/js/logik_kanter.js
@@ -5,6 +5,10 @@ const refs = {
   list: document.querySelector('.categories-books'),
 };
 
+const arrayError =
+  'Sorry, there are no books matching the selected category. Please select something else.';
+const fechError = 'Sorry, something went wrong. Try again!';
+
 refs.categories.addEventListener('click', onCategoriesClick);
 
 let currentCategory = '';
@@ -15,13 +19,27 @@ function onCategoriesClick(event) {
   }
   currentCategory = event.target.textContent.replaceAll(' ', '%20');
 
-  searchService(currentCategory).then(resp => {
-    refs.list.innerHTML = createMarkup(resp.data);
-  });
+  searchService(currentCategory)
+    .then(resp => {
+      if (resp.data.length === 0) {
+        throw new Error(arrayError);
+        // throw new Error(Notify.info(arrayError));
+      }
+      refs.list.innerHTML = createMarkup(resp.data);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        console.log(fechError);
+        // Notify.failure(fechError);
+      } else if (error.request) {
+        console.log(fechError);
+        // Notify.failure(fechError);
+      }
+    });
 }
 
 async function searchService(categoryValue) {
-  const URL = `https://books-backend.p.goit.global/books/category?category=${categoryValue}`;
+  const URL = `https://books-backend.p.goit.global/books/category?category123=${categoryValue}`;
 
   const data = await axios.get(URL);
   return data;
