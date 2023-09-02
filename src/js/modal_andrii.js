34d977cfbb2932612fafc8df390b511e47dec436
IwanovAS <127105553+IwanovAS@users.noreply.github.com>
2023-09-02 18:58:16 +0300

    Update modal_andrii.js

--- src/js/modal_andrii.js
+++ src/js/modal_andrii.js
@@ -1,25 +1,9 @@
-const backDrop = document.querySelector('#book-modal');
-const closeBtn = document.querySelector('.modal__close-btn');
-const addBookBtn = document.querySelector('.add-bookBtn');
-const removeNotification = document.querySelector('.removeNotification');
-const addNotification = document.querySelector('.addNotification');
-const notification = document.querySelector('.notification');
-const commonListRef = document.querySelector('.common-list');
-let modalContent = document.querySelector('.modal__content');
-let idToLocaleStorege = null;
-let arrToLocaleStoreg = [];
-
-addBookBtn.addEventListener('click', onAddBookClick);
-closeBtn.addEventListener('click', onBtnCloseClick);
-
-commonListRef.addEventListener('click', onClick);
-console.log(commonListRef);
-
 const commonListRef = document.querySelector('.common-list');
 const modalContent = document.querySelector('.modal__content');
 const backDrop = document.querySelector('#book-modal');
 
 commonListRef.addEventListener('click', onClick);
+console.log(commonListRef);
 
 function onClick(e) {
   if (e.target.className !== 'book-img') {
@@ -40,13 +24,7 @@ async function fetchBook(bookId) {
   return data;
 }
 
-function addModalMarkup({
-  author,
-  title,
-  description,
-  book_image,
-  amazon_product_url,
-}) {
+function addModalMarkup({ author, title, description, book_image }) {
   const card = `<div class="modal__img-container"> 
         <img src="${book_image}" alt="${title}" class="modal__img"> 
       </div> 
@@ -54,45 +32,40 @@ function addModalMarkup({
         <h2 class="modal__title">${title}</h2> 
         <p class="modal__author">${author}</p> 
         <p class="modal__book-desc">${description}</p> 
-        <ul class="modal__list"> 
-          <li class="modal__item"><a href="${amazon_product_url}" class="amazon-link"><img class="store-link-img amazon-img" src="${title}" alt=""></a></li> 
-          <li class="modal__item"><a href="${title}" class="app-book-link"><img class="store-link-img" src="${title}" alt=""></a></li> 
-          <li class="modal__item"><a href="${title}" class="book-shop-link"><img class="store-link-img" src="${title}" alt=""></a></li> 
-        </ul> 
+        // <ul class="modal__list"> 
+        //   <li class="modal__item"><a href="${amazon_product_url}" class="amazon-link"><img class="store-link-img amazon-img" src="${title}" alt=""></a></li> 
+        //   <li class="modal__item"><a href="${title}" class="app-book-link"><img class="store-link-img" src="${title}" alt=""></a></li> 
+        //   <li class="modal__item"><a href="${title}" class="book-shop-link"><img class="store-link-img" src="${title}" alt=""></a></li> 
+        // </ul> 
        
 </div>`;
   return card;
 }
 
-function onBtnCloseClick(e) {
-  console.log(e.code);
-  // console.log(e.currentTarget)
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.getElementById('book-modal');
+  const closeButton = document.querySelector('.modal__close-btn');
+  const modalContent = document.querySelector('.modal__content');
 
-  if (e.code === 'Escape') {
-    backDrop.removeEventListener('keydown', onBtnCloseClick);
-    backDrop.classList.add('is-hidden');
+  function closeModal() {
+    modal.classList.add('is-hidden');
   }
-  if (e.currentTarget === e.target) {
-    backDrop.classList.add('is-hidden');
-  }
-  if (e.target.classList.contains('modal__close-img')) {
-    backDrop.classList.add('is-hidden');
-  }
-}
 
-function onAddBookClick(res) {
-  notification.classList.toggle('hidden');
-  removeNotification.classList.toggle('hidden');
-  addNotification.classList.toggle('hidden');
+  closeButton.addEventListener('click', closeModal);
 
-  if (addNotification.classList.contains('hidden')) {
-    arrToLocaleStoreg.push(idToLocaleStorege);
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+  });
 
-    localStorage.setItem('shopping-list', JSON.stringify(arrToLocaleStoreg));
-  }
-  if (removeNotification.classList.contains('hidden')) {
-    const arrTofilter = JSON.parse(localStorage.getItem('shopping-list'));
-    const filtredArr = arrTofilter.filter(id => id !== idToLocaleStorege);
-    localStorage.setItem('shopping-list', JSON.stringify(filtredArr));
-  }
-}
+  modal.addEventListener('click', function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  modalContent.addEventListener('click', function (event) {
+    event.stopPropagation();
+  });
+});
