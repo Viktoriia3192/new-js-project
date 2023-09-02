0977fa2348477d9ad5ea2ac21c7bfb66d7a89c58
Daryna Holikova <dgolikova0@gmail.com>
2023-09-02 11:21:45 +0100

    styles

--- src/js/support_daryna.js
+++ src/js/support_daryna.js
@@ -10,7 +10,7 @@ data.forEach(({title, url, img}) => {
 function doMarkUp(title, url, img) {
     number += 1
     console.log("Image Path:", img);
-    const markup = `<li><p>0${number}.</p><a href="${url}"><img class="fond-img" srcset="${img}" alt="${title}"></a></li>`
+    const markup = `<li class="fonds-items"><p class="fond-num">0${number}.</p><a href="${url}"><img class="fond-img" srcset="${img}" alt="${title}"></a></li>`
     fondsList.insertAdjacentHTML("beforeend", markup)
     
 }
