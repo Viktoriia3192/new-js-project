f6c7b292ea7d07769123fd8c9cde4a91336ea8b0
Alex <alexlipskyi@icloud.com>
2023-09-02 19:36:59 +0300

    test2

--- src/js/best_sellers_api.js
+++ src/js/best_sellers_api.js
@@ -3,14 +3,16 @@ import axios from "axios";
 const baseURL = "https://books-backend.p.goit.global/books/top-books";
 export async function fetchBooksData () {
 
+  console.log("get api");
     try {
-
+    
         const response = await axios.get(baseURL);
-        
+        console.log("succsess");
         return response.data;
+
     } catch(error) {
 
-      throw  error;
+      console.error("An error occurred while fetching data:", error);
     }
    
 
