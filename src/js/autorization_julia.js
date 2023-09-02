9321f4bd7fea9934f22f0ec97698dc467bbf5c15
Viktoriia3192 <vikysja9231@gmail.com>
2023-09-02 17:47:04 +0100

    Update autorization_julia.js

--- src/js/autorization_julia.js
+++ src/js/autorization_julia.js
@@ -1,6 +1,5 @@
-import * as firebase from 'firebase/app';
+// import * as firebase from 'firebase/app';
 import { initializeApp } from 'firebase/app';
-// import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import axios from 'axios';
