d4e9f25661d36051d58210651343e073073b6a67
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-09-03 11:22:47 +0200

    fix #1 (api see more)

--- src/js/best_sellers_lipskii.js
+++ src/js/best_sellers_lipskii.js
@@ -1,75 +1,72 @@
-import {save,load} from './localStorageService';
-import {fetchBooksData} from './best_sellers_api';
+import { save, load } from './localStorageService';
+import { fetchBooksData } from './best_sellers_api';
 
-const list = document.querySelector(".common-list");
+const list = document.querySelector('.common-list');
 let length = window.innerWidth < 767 ? 1 : window.innerWidth < 1439 ? 3 : 5;
 
-window.addEventListener("resize", checkResize);
-
+window.addEventListener('resize', checkResize);
 
 fetchBooksData()
-.then(data => {
-   
-  save("data",data);
-  renderMarkup(data);
+  .then(data => {
+    save('data', data);
+    renderMarkup(data);
+  })
+  .catch(error => console.log(error));
 
-})
-.catch(error => console.log(error));
+function checkResize() {
+  // Check wether we should render markup or not.
 
-function checkResize() {  // Check wether we should render markup or not.
-         
-   let currentLength = window.innerWidth < 767 ? 1 : window.innerWidth < 1439 ? 3 : 5; 
- 
-   if (currentLength!=length) {
- 
+  let currentLength =
+    window.innerWidth < 767 ? 1 : window.innerWidth < 1439 ? 3 : 5;
+
+  if (currentLength != length) {
     length = currentLength;
 
-     const data = load("data");
+    const data = load('data');
 
     renderMarkup(data);
-   }
-
+  }
 }
 
-function renderMarkup(categories) {  //render categories
+function renderMarkup(categories) {
+  //render categories
 
-     let markup = '';
-       
-   for( let category of categories) {
-      
-        const {books,list_name} = category;
-        markup+=` 
+  let markup = '';
+
+  for (let category of categories) {
+    const { books, list_name } = category;
+    markup += ` 
         <li class="common-item">
         <h2 class="common-title">${list_name}</h2>
         <ul class="book-list">
-            ${booksRender(books,length)}
+            ${booksRender(books, length)}
         </ul>
-        <button type="button" class="showMore-btn" name=${list_name}>see more</button>
-    </li>`
-       
-    } 
+        <button type="button" class="showMore-btn" name="${list_name}">see more</button>
+    </li>`;
+  }
 
-    function booksRender(books,length) {  // render books inside the category
-      
-          let bookMarkup = '';
-         
-         for(let i =0;i<length;i+=1) {
-            const {book_image,title,author,_id} = books[i];
-              bookMarkup+=`
+  function booksRender(books, length) {
+    // render books inside the category
+
+    let bookMarkup = '';
+
+    for (let i = 0; i < length; i += 1) {
+      const { book_image, title, author, _id } = books[i];
+      bookMarkup += `
               <li class="book-item" data-id="${_id}">
               <a href="#" class="book-link">
-              <img src="${book_image || '../images/default_image.jpg'}" alt="${title}" class="book-img" data-id="${_id}"> 
+              <img src="${
+                book_image || '../images/default_image.jpg'
+              }" alt="${title}" class="book-img" data-id="${_id}"> 
               <h3 class="book-title">${title}</h3>
               <p class="book-author">${author}</p>
               </a>
               </li>
-            `
-         }
-
-         return bookMarkup;
-
+            `;
     }
 
-     list.innerHTML=markup;
+    return bookMarkup;
+  }
 
+  list.innerHTML = markup;
 }
