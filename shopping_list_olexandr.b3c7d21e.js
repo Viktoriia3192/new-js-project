dd0593db69ac6c85139189a60cba951fcc7a55b9
Viktoriia3192 <127699121+Viktoriia3192@users.noreply.github.com>
2023-09-04 17:49:28 +0000

    Deploying to gh-pages from @ Viktoriia3192/new-js-project@a45e005cc22121042f609c71a820dd788aa59050 🚀

---
+++ shopping_list_olexandr.b3c7d21e.js
@@ -0,0 +1,3 @@
+var e,n,r,o;e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},r={},null==(o=e.parcelRequired7c6)&&((o=function(e){if(e in n)return n[e].exports;if(e in r){var o=r[e];delete r[e];var i={id:e,exports:{}};return n[e]=i,o.call(i.exports,i,i.exports),i.exports}var d=Error("Cannot find module '"+e+"'");throw d.code="MODULE_NOT_FOUND",d}).register=function(e,n){r[e]=n},e.parcelRequired7c6=o),o("6JpON"),o("knWGd"),o("ksGdE"),o("fxge8"),o("E6hPi");//# sourceMappingURL=shopping_list_olexandr.b3c7d21e.js.map
+
+//# sourceMappingURL=shopping_list_olexandr.b3c7d21e.js.map
